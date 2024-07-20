3da825a23a2515edc6cd7a0d20ad4334d090947b
maya <maya.alieva.git@tekschool.us>
2024-07-20 16:10:49 -0500

    make some changes here

---
+++ src/Hello/Word/ClassThree.java
@@ -0,0 +1,14 @@
+package Hello.Word;
+
+public class ClassThree {
+
+    public static void main(String[] args){
+
+        String a = " Alijon Ali";
+
+        System.out.println("Alijon Ali");
+
+
+    }
+
+}
